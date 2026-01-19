1d0c19c8f195d1b1c352fc16d8cd0e022b087679
Soliprem <franci.solidoro@gmail.com>
2026-01-19 10:58:04 +0100

    feat(flake): init flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A lightweight Subsonic TUI music player built in Go";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          subtui = pkgs.buildGoModule {
+            pname = "subtui";
+            version = "unstable-${self.shortRev or self.dirtyShortRev or "dev"}";
+
+            src = lib.cleanSource self;
+            vendorHash = "sha256-PR1dY5e9UKqeM+EVNzDEXVHr3nPV+9meMnsKy1ZsxVo=";
+
+            proxyVendor = true; 
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.mpv ];
+
+            ldflags = [ "-s" "-w" ];
+
+            meta = with lib; {
+              description = "A lightweight Subsonic TUI music player with scrobbling and mpv backend";
+              homepage = "https://github.com/MattiaPun/SubTUI";
+              license = licenses.mit;
+              mainProgram = "subtui";
+            };
+          };
+
+          default = self.packages.${system}.subtui;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.subtui ];
+            
+            nativeBuildInputs = with pkgs; [
+              go
+              gopls
+              go-tools
+            ];
+
+            shellHook = ''
+              export CGO_ENABLED=1
+            '';
+          };
+        });
+    };
+}
